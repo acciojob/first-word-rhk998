1d02127aea97b0a602b7ca5ec0f36a7a5c610d29
rhk998 <130037816+rhk998@users.noreply.github.com>
2024-10-13 19:44:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	str = str.trim();
+
+	const spaceIndex = str.indexOf(' ');
+
+	return spaceIndex === -1 ? str : str.substring(0, spaceIndex);
+
 }
 
 // Do not change the code below
